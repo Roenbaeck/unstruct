4ffbabfd0819b97509693977ca360e8be8b326b9
Lars Rönnbäck <lars.ronnback@anchormodeling.com>
2022-05-06 14:33:46 +0200

    Parsing of multiple files

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use roxmltree;
 use config;
 use clap::Parser;
 use std::collections::HashMap;
+use glob::glob;
 
 /// Simple program to greet a person
 #[derive(Parser, Debug)]
@@ -14,27 +15,39 @@ struct Args {
 }
 
 fn main() {
-    let args = Args::parse();
-    // read from command line later
-    let filename = args.filename;
-    println!("Filename: {}", &filename);
-
+    // read the config containing the mapping between elements and columns
     let cfg = config::Config::builder().add_source(config::File::with_name("unstruct.ini")).build().unwrap();
     let map = cfg.try_deserialize::<HashMap<String, String>>().unwrap();
+
+    // parse the arguments to get the filename glob pattern
+    let args = Args::parse();
+    let filename = args.filename;
+    println!("Finding files matching: {}", &filename);
+
+    // use the glob to find matching files
+    for entry in glob(&filename).expect("Failed to read glob pattern!") {
+        match entry {
+            Ok(path) => {
+                println!("Parsing the file: {:?}", &path.display());
+                let contents = fs::read_to_string(&path).expect("Something went wrong reading the file");
+                let doc = roxmltree::Document::parse(&contents).unwrap();
+                for element in doc.descendants() {
+                    match map.get(&element.tag_name().name().to_string()) {
+                        Some(mapping) => {
+                            println!("Hit: {} = {}",&mapping, &element.text().unwrap());
+                        }
+                        None => ()
+                    };
+                }
+            },
+            Err(e) => println!("{:?}", e),
+        }
+    }
+
+
     // println!("Map: {:?}", map);
 
-    let contents = fs::read_to_string(filename)
-        .expect("Something went wrong reading the file");
-    let doc = roxmltree::Document::parse(&contents).unwrap();
 
-    for element in doc.descendants() {
-        match map.get(&element.tag_name().name().to_string()) {
-            Some(mapping) => {
-                println!("Hit: {} = {}",&mapping, &element.text().unwrap());
-            }
-            None => ()
-        };
-    }
     // test
     //let elem = doc.descendants().find(|n| n.has_tag_name("CDR")).unwrap();
     //let elem = elem.descendants().find(|n| n.has_tag_name("recordType")).unwrap();
