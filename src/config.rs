8dde4db07f691c9c6661744bfac78085676783aa
Lars Rönnbäck <lars.ronnback@anchormodeling.com>
2022-06-17 13:49:22 +0200

    Working, but nesting is not finished

--- src/config.rs
+++ src/config.rs
@@ -1,31 +1,66 @@
 use pest::Parser;
 use pest_derive::Parser;
-use pest::iterators::Pair;
+use pest::iterators::{Pair, Pairs};
+use std::{collections::HashMap, hash::Hash};
 
 #[derive(Parser)]
 #[grammar = "parser.pest"] // relative to src
 struct UnstructParser;
 
-pub fn parse(configuration: &str) {
-    println!("The configuration is:\n{}", configuration);
-    let parser = UnstructParser::parse(Rule::block, configuration.trim()).expect("Parsing error");
-    for directive in parser {
-        println!("{:?}", &directive);
-        let mut column_name: Option<String> = None; 
-        let mut xml_name: Option<String> = None; 
-        for column_or_xml in directive.into_inner() {
-            match column_or_xml.as_rule() {
-                Rule::column_name => {
-                    column_name = Some(column_or_xml.as_str().to_owned());
+#[derive(Debug)]
+pub struct Directive {
+    pub level: usize, 
+    pub column_name: String
+}
+
+pub fn block_recurse(remainder: Pairs<Rule>, matcher: &mut HashMap<String, Directive>, level: usize) {
+    for directive_or_block in remainder {
+        match directive_or_block.as_rule() {
+            Rule::directive => {
+                let mut column_name: Option<String> = None; 
+                let mut xml_name: Option<String> = None; 
+                for column_or_xml in directive_or_block.into_inner() {
+                    match column_or_xml.as_rule() {
+                        Rule::column_name => {
+                            column_name = Some(column_or_xml.as_str().to_owned());
+                        }
+                        Rule::xml_name => { 
+                            xml_name = Some(column_or_xml.as_str().to_owned());
+                        }, 
+                        _ => {
+                            println!("Parsing error: {:?}", column_or_xml);
+                        }
+                    }
                 }
-                Rule::xml_name => { 
-                    xml_name = Some(column_or_xml.as_str().to_owned());
-                }, 
-                _ => {
-                    println!("Parsing error: {:?}", column_or_xml);
-                }
+                // println!("{} = {}", column_name.as_ref().unwrap(), xml_name.as_ref().unwrap());
+                matcher.insert(
+                    {
+                        let mut chars = xml_name.as_ref().unwrap().chars();
+                        chars.next();
+                        chars.next_back();
+                        chars.as_str().to_owned()
+                    }, 
+                    Directive {
+                        level, 
+                        column_name: column_name.as_ref().unwrap().to_owned()
+                    }
+                );
+            }
+            Rule::block => {
+                block_recurse(directive_or_block.into_inner(), matcher, level + 1);
+            }
+            _ => {
+                println!("Parsing error: {:?}", directive_or_block);
             }
         }
-        println!("{} = {}", column_name.as_ref().unwrap(), xml_name.as_ref().unwrap());
     }
+}
+
+pub fn parse(configuration: &str) -> HashMap<String, Directive> {
+    // println!("The configuration is:\n{}", configuration);
+    let remainder = UnstructParser::parse(Rule::block, configuration.trim()).expect("Parsing error");
+    let mut matcher: HashMap<String, Directive> = HashMap::default();
+    block_recurse(remainder, &mut matcher, 0);
+    // println!("{:?}", &matcher);
+    matcher
 }
--- src/main.rs
+++ src/main.rs
@@ -4,24 +4,50 @@ use clap::Parser;
 use std::collections::HashMap;
 use glob::glob;
 use unstruct::config::parse;
-use std::fs::{read_to_string};
+use std::fs::{File, read_to_string};
+use std::io::Write;
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 struct Args {
     #[clap(short, long)]
     filename: String,
+    #[clap(short, long)]
+    outfile: String
 }
+
+#[derive(Debug)]
+enum Match {
+    Value(String),
+    Nothing
+}
+
+const DELIMITER: char = '\t';
+const TERMINATOR: char = '\n';
 
 fn main() {
     // read the config containing the mapping between elements and columns
     let configuration = read_to_string("unstruct.parser").unwrap();
-    parse(&configuration);
+    let matcher = parse(&configuration);
+    let mut result: HashMap<String, Match> = HashMap::default();
+    let mut header: Vec<String> = Vec::default(); // repeatable list, since .values is not
+    for directive in matcher.values() {
+        header.push(directive.column_name.to_owned());
+    }            
 
     // parse the arguments to get the filename glob pattern
     let args = Args::parse();
     let filename = args.filename;
+    let outfile = args.outfile;
     println!("Finding files matching: {}", &filename);
+    println!("Results are stored in: {}", &outfile);
+
+    let mut output = File::create(outfile).unwrap();
+    for column_header in &header {
+        write!(output, "{}", &column_header).expect("Cannot write to output file");
+        write!(output, "{}", DELIMITER).expect("Cannot write to output file");
+    }            
+    write!(output, "{}", TERMINATOR).expect("Cannot write to output file");
 
     // use the glob to find matching files
     for entry in glob(&filename).expect("Failed to read glob pattern") {
@@ -30,17 +56,36 @@ fn main() {
                 println!("Parsing the file: {:?}", &path.display());
                 let contents = fs::read_to_string(&path).expect("Something went wrong reading the file");
                 let doc = roxmltree::Document::parse(&contents).expect("Could not parse the xml");
+                // clear the results between each file
+                for column_header in &header {
+                    result.insert(column_header.to_owned(), Match::Nothing);
+                }            
                 for element in doc.descendants() {
-                    /* 
-                    match map.get(&element.tag_name().name().to_string()) {
-                        Some(column) => {
-                            result.insert(column, element.text().unwrap().to_owned());
-                            println!("Hit: {} = {}", column, result.get(column).unwrap());
-                        }
-                        None => ()
-                    };
-                    */
+                    if element.is_element() {
+                        // println!("Looking for: {}", &element.tag_name().name().to_string());
+                        match matcher.get(&element.tag_name().name().to_string()) {
+                            Some(column) => {
+                                result.insert(
+                                    column.column_name.to_owned(), 
+                                    Match::Value(element.text().unwrap().to_owned())
+                                );
+                            }
+                            None => ()
+                        };
+                    }
                 }
+                for column_header in &header {
+                    match result.get(column_header).unwrap() {
+                        Match::Value(column_value) => {
+                            write!(output, "{}", column_value).expect("Cannot write to output file");
+                            write!(output, "{}", DELIMITER).expect("Cannot write to output file");                    
+                        }
+                        Match::Nothing => {
+                            write!(output, "{}", DELIMITER).expect("Cannot write to output file");                    
+                        }
+                    };
+                }            
+                write!(output, "{}", TERMINATOR).expect("Cannot write to output file");
             },
             Err(e) => println!("{:?}", e),
         }
